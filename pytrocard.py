41e4be2abc6368611aaf05019c48293648ee52cd
Pedro Amorim <pedrohasa@gmail.com>
2017-09-23 20:52:00 -0400

    Fix formatting
    Print two decimal places for recharge_value appropriately with the
    format function

--- pytrocard.py
+++ pytrocard.py
@@ -4,14 +4,9 @@ fare = 2.75
 
 
 while True:
-	if (round(1.05 * recharge_value, 2) + initial_balance) % fare == 0:
-		while True:
-			if len(str(recharge_value)) < 4:
-				recharge_value = str(recharge_value) + '0'
-			else:
-				break
+	if (round(1.05 * recharge_value, 2) + initial_balance) % fare == 0:		
 		message = "Minimum recharge value: "
-		message += str(recharge_value)
+		message += str(format(recharge_value, '.2f'))
 		print(message)
 		break
 	else:
